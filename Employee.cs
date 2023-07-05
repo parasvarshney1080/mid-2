4694d33e0b7499262233b928ff5d2807270dfed7
parasvarshney1080 <49190931+parasvarshney1080@users.noreply.github.com>
2023-07-05 22:56:06 +0530

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -3,4 +3,7 @@ public class Employee
     public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
+    public string Gender { get; set; }
+    public string Profession { get; set; }
+    public string Languages { get; set; }
 }
