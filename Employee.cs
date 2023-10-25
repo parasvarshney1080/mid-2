33823b313f8a6f9bf614ae3e7918564b08529822
parasvarshney1080 <49190931+parasvarshney1080@users.noreply.github.com>
2023-10-25 09:45:37 +0530

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,10 +1,12 @@
-public class Employee
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product_UI.Controllers
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Gender { get; set; }
-    public string Profession { get; set; }
-    public string Languages { get; set; }
-    public string Country { get; set; }
+    public class ProductUIController : Controller
+    {
+        public IActionResult ProductUI()
+        {
+            return View();
+        }
+    }
 }
