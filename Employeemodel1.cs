f96edc3f7ff20ab08389f44beefd2ba0c1288de5
parasvarshney1080 <49190931+parasvarshney1080@users.noreply.github.com>
2023-11-02 13:41:24 +0530

    Update Employeemodel1.cs

--- Employeemodel1.cs
+++ Employeemodel1.cs
@@ -1,9 +1,31 @@
-model/employee.cs
-
-public class Employee
+public IActionResult Index()
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Profession { get; set; }
-}
+    var jwtToken = Request.Cookies["jwtToken"];
 
+    if(!string.IsNullOrEmpty(jwtToken))
+    {
+        // Token is present, you can use it for authentication
+
+        // Make an AJAX request to fetch data from your API using the token
+        $.ajax({
+            url: 'https://localhost:7102/api/product',
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + jwtToken // Include the token in the headers
+            },
+            success: function (data) {
+                // Data is successfully fetched from the API, you can use it in your view
+                return View(data); // Pass the data to your view for rendering
+            },
+            error: function (error) {
+                console.error('Error fetching data from API:', error);
+                // Handle the error, for example, display a message to the user
+            }
+        });
+    }
+    else
+    {
+        // Token is not present, handle this case
+        return RedirectToAction("Login", "Home"); // Redirect to login page
+    }
+}
